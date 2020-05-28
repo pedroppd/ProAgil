e2d816b804edfc712b9b3e5e0f880ebb7bafe8c7
Pedro Dantas <pedro_pdantas@hotmail.com>
2020-05-28 20:08:31 -0300

    First commit

---
+++ ProAgil.WebAPI/Controllers/ValueController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        public DataContext _context { get; }
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {   
+                List<Evento> results = await this._context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status404NotFound, "Banco de dados falhou :(");
+            }
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {  
+                Evento result = await this._context.Eventos.FirstOrDefaultAsync(eventos => eventos.EventoId == id);
+                return Ok(result);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status404NotFound, "Erro ao buscar :" + id);
+            }
+        }
+    }
+}
